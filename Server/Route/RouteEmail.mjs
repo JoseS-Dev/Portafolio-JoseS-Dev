e06a6f929f4cd16d1e405f117bc9deace4a5499b
JoSantana0005 <joseasantana05@gmail.com>
2025-10-03 23:29:33 -0400

    refactor: :recycle: Remove email sending functionality and related configurations

--- Server/Route/RouteEmail.mjs
+++
@@ -1,8 +0,0 @@
-import {Router} from 'express';
-import { SendEmail } from '../Controller/SendEmail.mjs';
-
-const router = Router();
-export const RouteEmail = router;
-
-// Ruta de email
-router.post('/email', SendEmail.prototype.sendEmail);
